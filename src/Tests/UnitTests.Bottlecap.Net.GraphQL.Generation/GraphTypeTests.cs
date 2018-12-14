43443713032ac208b2816a97ca9a4cbb003f381f
BottlecapDave <me@davidskendall.co.uk>
2018-12-14 20:04:05 +0000

    Added unit tests around IsNullable override

--- src/Tests/UnitTests.Bottlecap.Net.GraphQL.Generation/GraphTypeTests.cs
+++ src/Tests/UnitTests.Bottlecap.Net.GraphQL.Generation/GraphTypeTests.cs
@@ -1,5 +1,6 @@
 ﻿using Bottlecap.Net.GraphQL.Generation;
 using Bottlecap.Net.GraphQL.Generation.Attributes;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -18,11 +19,49 @@ namespace UnitTests.Bottlecap.Net.GraphQL.Generation
             ActAndAssertGeneratedResult(generator, nameof(Generate_When_NameOverrideSpecified_Then_GenerateClassHasNameOverride));
         }
 
+        [Fact]
+        public void Generate_When_IsNullableSpecified_IsNullable_Then_GenerateClassHasNullableProperty()
+        {
+            // Arrange
+            var generator = new Generator();
+            generator.RegisterGraphTypes(new TypeDefinition(typeof(GenerateWhenIsNullableSpecifiedIsNullableThenGenerateClassHasNullableProperty)));
+
+            // Assert
+            ActAndAssertGeneratedResult(generator, nameof(Generate_When_IsNullableSpecified_IsNullable_Then_GenerateClassHasNullableProperty));
+        }
+
+        [Fact]
+        public void Generate_When_IsNullableSpecified_IsNotNullable_Then_GenerateClassHasNonNullableProperty()
+        {
+            // Arrange
+            var generator = new Generator();
+            generator.RegisterGraphTypes(new TypeDefinition(typeof(GenerateWhenIsNullableSpecifiedIsNotNullableThenGenerateClassHasNonNullableProperty)));
+
+            // Assert
+            ActAndAssertGeneratedResult(generator, nameof(Generate_When_IsNullableSpecified_IsNotNullable_Then_GenerateClassHasNonNullableProperty));
+        }
+
+        #region Classes
+
         [GraphType]
         public class GenerateWhenNameOverrideSpecifiedThenGenerateClassHasNameOverride
         {
             [GraphTypeProperty(Name="NewName")]
             public string Name { get; set; }
         }
+
+        public class GenerateWhenIsNullableSpecifiedIsNullableThenGenerateClassHasNullableProperty
+        {
+            [GraphTypeProperty(IsNullable=true)]
+            public string Name { get; set; }
+        }
+
+        public class GenerateWhenIsNullableSpecifiedIsNotNullableThenGenerateClassHasNonNullableProperty
+        {
+            [GraphTypeProperty(IsNullable = false)]
+            public string Name { get; set; }
+        }
+
+        #endregion
     }
 }
